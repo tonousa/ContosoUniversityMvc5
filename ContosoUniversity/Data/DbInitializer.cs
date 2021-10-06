3e7829785d6fdfa271194b47230bf15ee8d57008
mark saboya <mark.saboya@dps.texas.gov>
2021-10-06 16:00:33 -0500

    Add project files.

---
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Students.Any())
+            {
+                return; //db has been seeded
+            }
+
+            var students = new Student[]
+            {
+
+            }
+        }
+    }
+}
