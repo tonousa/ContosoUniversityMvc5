fd5c49427ba49b4db90a375099ce95b8776f2f31
mark saboya <mark.saboya@dps.texas.gov>
2021-10-08 16:48:31 -0500

    partially added Instructor class

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name ="Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        []
+        public DateTime HireDate { get; set; }
+    }
+}
