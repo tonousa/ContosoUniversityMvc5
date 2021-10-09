2396e4ab74c3858e426457cc8e75b7300f4af1ed
mark saboya <mark.saboya@dps.texas.gov>
2021-10-09 16:58:58 -0500

    fixed db initializer

--- ContosoUniversity/Data/DbInitializer.cs
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -56,7 +56,9 @@ namespace ContosoUniversity.Data
 
             var departments = new Department[]
             {
-                new Department {Name="English", Budget=350000, StartDate=DateTime.Parse("2007-09-01")},
+                new Department {Name="English", Budget=350000, 
+                    StartDate=DateTime.Parse("2007-09-01"), 
+                    InstructorID = instructors.Single(i => i.LastName == "Abercrombie").ID },
                 new Department { Name = "Mathematics", Budget = 100000,
                     StartDate = DateTime.Parse("2007-09-01"),
                     InstructorID  = instructors.Single( i => i.LastName == "Fakhouri").ID },
@@ -71,17 +73,25 @@ namespace ContosoUniversity.Data
             {
                 context.Departments.Add(d);
             }
+            context.SaveChanges();
 
 
             var courses = new Course[]
             {
-                new Course {CourseID=1050, Title="Chemistry", Credits=3},
-                new Course{CourseID=4022,Title="Microeconomics",Credits=3},
-                new Course{CourseID=4041,Title="Macroeconomics",Credits=3},
-                new Course{CourseID=1045,Title="Calculus",Credits=4},
-                new Course{CourseID=3141,Title="Trigonometry",Credits=4},
-                new Course{CourseID=2021,Title="Composition",Credits=3},
-                new Course{CourseID=2042,Title="Literature",Credits=4}
+                new Course {CourseID=1050, Title="Chemistry", Credits=3, 
+                    DepartmentID = departments.Single(d => d.Name == "Engineering").DepartmentID},
+                new Course{CourseID=4022,Title="Microeconomics",Credits=3,
+                    DepartmentID = departments.Single(d => d.Name == "Economics").DepartmentID},
+                new Course{CourseID=4041,Title="Macroeconomics",Credits=3,
+                    DepartmentID = departments.Single( s => s.Name == "Economics").DepartmentID},
+                new Course{CourseID=1045,Title="Calculus",Credits=4,
+                    DepartmentID = departments.Single( s => s.Name == "Mathematics").DepartmentID},
+                new Course{CourseID=3141,Title="Trigonometry",Credits=4,
+                    DepartmentID = departments.Single( s => s.Name == "Mathematics").DepartmentID},
+                new Course{CourseID=2021,Title="Composition",Credits=3,
+                    DepartmentID = departments.Single( s => s.Name == "English").DepartmentID},
+                new Course{CourseID=2042,Title="Literature",Credits=4,
+                    DepartmentID = departments.Single( s => s.Name == "English").DepartmentID}
             };
             foreach (Course c in courses)
             {
